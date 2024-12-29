e5422619154ed1083c809d88b7aef85d60ba1b55
Gratonic <enchantelic@proton.me>
2024-12-29 14:28:46 -0800

    Fixed a major issue with the hs_prompts code. I am going to rewrite it because it is currently spaghetti code.

--- Hackesoup_V1/Main/Soup/Interfaces/hs_mi.py
+++ Hackesoup_V1/Main/Soup/Interfaces/hs_mi.py
@@ -1,6 +1,7 @@
-# Imports
+# :: Imports :: #
 
 # PIP Modules
+import colorama
 import sys
 import os
 # Used to import the custom modules
@@ -16,3 +17,54 @@ sys.path.append(modules_dir)
 # Imports the custom modules
 hs_menus = importlib.import_module('hs_menus')
 hs_prompts = importlib.import_module('hs_prompts')
+
+# :: Global Variables :: #
+
+# Colour Objects, Used For Nicer Output
+reset = colorama.Fore.RESET
+blue = colorama.Fore.BLUE
+light_blue = colorama.Fore.LIGHTBLUE_EX
+cyan = colorama.Fore.CYAN
+light_cyan = colorama.Fore.LIGHTCYAN_EX
+red = colorama.Fore.RED
+light_red = colorama.Fore.LIGHTRED_EX
+green = colorama.Fore.GREEN
+light_green = colorama.Fore.LIGHTGREEN_EX
+yellow = colorama.Fore.YELLOW
+light_yellow = colorama.Fore.LIGHTYELLOW_EX
+magenta = colorama.Fore.MAGENTA
+light_magenta = colorama.Fore.LIGHTMAGENTA_EX
+white = colorama.Fore.WHITE
+gray = colorama.Fore.LIGHTBLACK_EX
+
+# ::  Special Functions :: #
+
+# Says "Goodbye!" to the user in German and exits the program
+def terminate_program():
+    print(f"{magenta}\n\nTschüss!{reset}")
+    exit()
+
+# :: UX Menu Interface Functions :: #
+
+# Builds the tool/main UX menu and asks the user for their tool choice
+def main_UX_menu() -> vars:
+    hs_menus.call_main_menu()
+    chosen_tool = hs_prompts.toolbox()
+    return chosen_tool
+
+def port_scanner_UX_menus():
+    try:
+        target = hs_prompts.target()
+        # TODO: Fix the issue (caused by something to do with the target and setup prompt functions) that is preventing the setup prompt from being called
+        # NOTE: When target is printed, it is both the ip entered and None
+        hs_menus.call_port_scanner_setup_1()
+        hs_prompts.setup()
+    except KeyboardInterrupt:
+        terminate_program()
+    
+port_scanner_UX_menus()
+
+port_scanner_UX_menus()
+# Handles the entire UX menu and returns a list of all the user input
+def UX_menu_interface() -> list:
+    pass
--- Hackesoup_V1/Main/Soup/Lib/Modules/hs_prompts.py
+++ Hackesoup_V1/Main/Soup/Lib/Modules/hs_prompts.py
@@ -20,81 +20,98 @@ light_magenta = colorama.Fore.LIGHTMAGENTA_EX
 white = colorama.Fore.WHITE
 gray = colorama.Fore.LIGHTBLACK_EX
 
-# Set to 1 and breaks out of loop if all requested user input is valid
-valid_choice = 0
-
 # Used for some functions to determine the amount of threads the user can safely use for the program
 def find_safe_thread_amount() -> int:
     thread_count = psutil.cpu_count(logical=True)
     thread_amount_allowed_for_program = round(int(thread_count / 2))
     return thread_amount_allowed_for_program
 
+# Says "Goodbye!" to the user in German and exits the program
+def terminate_program():
+    print(f"{magenta}\n\nTschüss!{reset}")
+    exit()
+
 # Toolbox Prompt
 def toolbox() -> int:
-    global valid_choice
-    while valid_choice == 0:
+    while True:
         tool = input(f"{magenta}Please Choose An Option From The Toolbox{reset} {green}[Ex: 3]{reset}: ")
         try:
             tool = int(tool)
             # 7 because of easter egg, number will be changed to a higher number in future versions
             if tool <= 7:
-                valid_choice = 1
                 return tool
             else:
                 print(f"{red}[!] Error{reset}: {light_red}Invalid Tool.{reset}")
+        except KeyboardInterrupt:
+            terminate_program()
         except:
             print(f"{red}[!] Error{reset}: {light_red}Invalid Tool.{reset}")
 
 # Tool Setup Prompt
 def setup() -> int:
-    global valid_choice
-    while valid_choice == 0:
+    while True:
         setup_opt = input(f"{magenta}Please Choose A Setup Option{reset} {green}[Ex: 2]{reset}: ") 
         try:
             setup_opt = int(setup_opt)
             # 5 is the last setup option for the largest setup menus, may change in future versions
             if setup_opt <= 13:
-                valid_choice = 1
                 return setup_opt
             else:
                 print(f"{red}[!] Error{reset}: {light_red}Invalid Setup Option.{reset}")
+        except KeyboardInterrupt:
+            terminate_program()
         except:
             print(f"{red}[!] Error{reset}: {light_red}Invalid Setup Option.{reset}")
 
 # Target Prompt - IP Address Only
-def target() -> int:
-    global valid_choice
-    while valid_choice == 0:
-        targ = input(f"{magenta}Please Specify a Target{reset} {green}[Ex: 74.203.143.35]{reset}: ")
+def target() -> str:
+    while True:
         try:
-            socket.gethostbyaddr(target)
-            valid_choice = 1
-            return targ
+            targ = input(f"{magenta}Please Specify a Target{reset} {green}[Ex: 74.203.143.35]{reset}: ")
+            octets = targ.split(".")
+            try:
+                good_octets = []
+                for octet in octets:
+                    octet = int(octet)
+                    if octet >= 1 and octet <= 255:
+                        good_octets.append(octet)
+                    else:
+                        print(f"{red}[!] Error{reset}: One Or More Octets Are Invalid.{reset}")
+                        break
+            except:
+                print(f"{red}[!] Error{reset}: One Or More Octets Are Invalid.{reset}")
+            # Returns the target if there are enough good octets
+            if len(good_octets) == 4:
+                return targ
+            else:
+                continue
+        except KeyboardInterrupt:
+            terminate_program()
         except:
             print(f"{red}[!] Error{reset}: {light_red}Invalid Target.{reset}")
 
 # Special Target Prompt - IP Address or Website
 def special_target() -> int:
-    global valid_choice
-    while valid_choice == 0:
+    while True:
         targ = input(f"{magenta}Please Specify a Target{reset} {green}[Ex: 74.203.143.35 or www.example.com]{reset}: ")
         # User provided target is more likely to be a website (domain name) than an IP address
         try:
             socket.gethostbyname(target)
-            valid_choice = 1
             return targ
+        except KeyboardInterrupt:
+            terminate_program()
         except:
             try:
                 socket.gethostbyaddr(target)
-                valid_choice = 1
                 return targ
+            except KeyboardInterrupt:
+                terminate_program()
             except:
                 print(f"{red}[!] Error{reset}: {light_red}Invalid Target.{reset}")
 
 # Port Prompt
 def port() -> int:
-    global valid_choice
-    while valid_choice == 0:
+    while True:
         prt = input(f"{magenta}Please Specify A Port {green}[Ex: 21]{reset}: ")
         try:
             prt = int(port)
@@ -104,15 +121,15 @@ def port() -> int:
             elif prt < 1:
                 print(f"{red}[!] Error{reset}: {light_red}Invalid Port.{reset}")
             else:
-                valid_choice = 1
                 return prt
+        except KeyboardInterrupt:
+            terminate_program()
         except:
             print(f"{red}[!] Error{reset}: {light_red}Invalid Port.{reset}")
 
 # Port Range Prompt
 def port_range() -> str:
-    global valid_choice
-    while valid_choice == 0:
+    while True:
         prt_range = input(f"{magenta}Please Specify A Port Range Seperated By Spaces{reset} {green}[Ex: 1-53]{reset}: ")
         try:
             prts = prt_range.split("-")
@@ -124,31 +141,31 @@ def port_range() -> str:
             elif start_prt < 1 or end_prt < 1:
                 print(f"{red}[!] Error{reset}: {light_red}Invalid Port Range.{reset}")
             else:
-                valid_choice = 1
                 return prt_range
+        except KeyboardInterrupt:
+            terminate_program()
         except:
             print(f"{red}[!] Error{reset}: {light_red}Invalid Port Range.{reset}")
 
 # Settings Prompt
 def settings() -> int:
-    global valid_choice
-    while valid_choice == 0:
+    while True:
         setting = input(f"Please Choose A Setting: ")
         try:
             int(setting)
             # 13 is the last possible menu option for the largest setting menus, may change in future versions
             if setting > 13:
-                valid_choice = 1
                 return setting
             else:
                 print(f"{red}[!] Error{reset}: {light_red}Invalid Setting.{reset}")
+        except KeyboardInterrupt:
+            terminate_program()
         except:
             print(f"{red}[!] Error{reset}: {light_red}Invalid Setting.{reset}")
 
 # Custom Payload File Prompt
 def custom_payload_file() -> str:
-    global valid_choice
-    while valid_choice == 0:
+    while True:
         pay_file_path = input(f"{magenta}Please Specify The Path To Your Payload File{reset} {green}[Ex: ./Payloads/your_payloads.txt]{reset}: ")
         try:
             # Splits the file path up for basic validation
@@ -158,48 +175,47 @@ def custom_payload_file() -> str:
             pay_file_contents = pay_file_contents.split(".")
             pay_file = pay_file_contents[-1]
             if pay_file == "txt":
-                valid_choice = 1
                 return pay_file_path
             elif pay_file == "text":
-                valid_choice = 1
                 return pay_file_path
             else:
                 print(f"{red}[!] Error{reset}: {light_red}Invalid File Path or File Type (file type must be {reset}{green}.txt or .text{reset}{light_red}).{reset}")
+        except KeyboardInterrupt:
+            terminate_program()
         except:
             print(f"{red}[!] Error{reset}: {light_red}Invalid File Path or File Type (file type must be {reset}{green}.txt or .text{reset}{light_red}).{reset}")
 
 # Timeout Amount Prompt
 def timeout_amount() -> int:
-    global valid_choice
-    while valid_choice == 0:
+    while True:
         timeout = input(f"{magenta}Please Specify The Timeout Amount{reset} {green}[Ex: '3' is 3 seconds]{reset}: ")
         try:
             timeout = int(timeout)
-            valid_choice = 1
             return timeout
+        except KeyboardInterrupt:
+            terminate_program()
         except:
             print(f"{red}[!] Error{reset}: {light_red}Invalid Input. Numbers Only!{reset}")
 
 # Thread Amount Prompt
 def thread_amount() -> int:
-    global valid_choice
     safe_thread_amount = find_safe_thread_amount()
-    while valid_choice == 0:
+    while True:
         thread_amount = input(f"{magenta}Please Specify The Thread Amount{reset} {green}[Ex: 4]{reset}: ")
         try:
             thread_amount = int(thread_amount)
             if thread_amount > safe_thread_amount:
                 print(f"{red}[!] Error{reset}: {light_red}Too Many Threads. You May Use Up To {reset}{green}{safe_thread_amount}{reset}{light_red} Threads For This Computer, Using More Could Overload Your CPU.{reset}")
-            valid_choice = 1
             return thread_amount
+        except KeyboardInterrupt:
+            terminate_program()
         except:
             print(f"{red}[!] Error{reset}: {light_red}Invalid Input. Numbers Only!{reset}")
 
 # Timeout and Thread Amount Prompt
 def timeout_and_thread_amount() -> list:
-    global valid_choice
     safe_thread_amount = find_safe_thread_amount()
-    while valid_choice == 0:
+    while True:
         timeout = input(f"{magenta}Please Specify The Timeout Amount{reset} {green}[Ex: '3' is 3 seconds]{reset}: ")
         thread_amount = input(f"{magenta}Please Specify The Thread Amount{reset} {green}[Ex: 4]{reset}: ")
         try:
@@ -208,16 +224,16 @@ def timeout_and_thread_amount() -> list:
             if thread_amount > safe_thread_amount:
                 print(f"{red}[!] Error{reset}: {light_red}Too Many Threads. You May Use Up To {reset}{green}{safe_thread_amount}{reset}{light_red} Threads For This Computer, Using More Could Overload Your CPU.{reset}")
             else:
-                valid_choice = 1
                 return [timeout, thread_amount]
+        except KeyboardInterrupt:
+            terminate_program()
         except:
             print(f"{red}[!] Error{reset}: {light_red}Invalid Input. Numbers Only!{reset}")
 
 # Custom Payload File Prompt + Timeout and Thread Amount Prompt
 def custom_payload_file_plus_timeout_and_thread_amount() -> list:
-    global valid_choice
     safe_thread_amount = find_safe_thread_amount()
-    while valid_choice == 0:
+    while True:
         pay_file_path = input(f"{magenta}Please Specify The Path To Your Payload File{reset} {green}[Ex: ./Payloads/your_payloads.txt]{reset}: ")
         timeout = input(f"{magenta}Please Specify The Timeout Amount{reset} {green}[Ex: '3' is 3 seconds]{reset}: ")
         thread_amount = input(f"{magenta}Please Specify The Thread Amount{reset} {green}[Ex: 4]{reset}: ")
@@ -232,6 +248,8 @@ def custom_payload_file_plus_timeout_and_thread_amount() -> list:
                 pass
             else:
                 print(f"{red}[!] Error{reset}: {light_red}Invalid File Path or File Type (file type must be {reset}{green}.txt or .text{reset}{light_red}).{reset}")
+        except KeyboardInterrupt:
+            terminate_program()
         except:
             print(f"{red}[!] Error{reset}: {light_red}Something Went Wrong During The File Validation Process.{reset}")
         try:
@@ -241,7 +259,8 @@ def custom_payload_file_plus_timeout_and_thread_amount() -> list:
                 print(f"{red}[!] Error{reset}: {light_red}Too Many Threads. You May Use Up To {reset}{green}{safe_thread_amount}{reset}{light_red} Threads For This Computer, Using More Could Overload Your CPU.{reset}")
             else:
                 # Safe to break the loop here because everything must go well to reach this point
-                valid_choice = 1
                 return [pay_file_path, timeout, thread_amount]
+        except KeyboardInterrupt:
+            terminate_program()
         except:
             print(f"{red}[!] Error{reset}: {light_red}Invalid Input! Numbers Only!{reset}")
