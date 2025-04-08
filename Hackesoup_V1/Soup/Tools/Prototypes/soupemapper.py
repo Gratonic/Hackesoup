d59dd515e55b9d5f24f806073dcc123d6d5aaa5f
Gratonic <enchantelic@proton.me>
2025-04-07 20:02:20 -0700

    test

--- Hackesoup_V1/Soup/Tools/Prototypes/soupemapper.py
+++ Hackesoup_V1/Soup/Tools/Prototypes/soupemapper.py
@@ -5,10 +5,22 @@ File Name: soupemapper.py
 Author(s): Gratonic (https://github.com/Gratonic)
 Written In: Python 3.10.12
 Dependencie(s): None (yet)
-Last Modified: April 5th, 2025
+Last Modified: April 7th, 2025
 
 # :: Description :: #
 
 This is the prototype for the Port Scanner / Network Mapper tool.
 """
 
+# :: Imports :: #
+import socket
+
+target = "192.168.1.1"
+port = 8080
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect_ex((target, port))
+message = "Hello Mr Router".encode()
+s.send(message)
+s.recv(1046)
+s.close()
