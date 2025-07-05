6e347f9f44975ca27f7fbfafe5798d28b8928339
Gratonic <189929383+Gratonic@users.noreply.github.com>
2025-07-05 01:11:45 -0500

    almost done

--- Soup/Tools/saifandor.py
+++ Soup/Tools/saifandor.py
@@ -88,9 +88,8 @@ def exit_program():
 with open("../Soup/Lib/Data/Input_Data/input.json", "r") as settings_file:
     settings = json.load(settings_file)
 
-class dns():
+class DNS():
     def __init__(self):
-        self.cache = {}
         self.resolver = dns.resolver.Resolver()
         self.resolver.nameservers = [
             "9.9.9.9",        "149.112.112.112", # Quad9
@@ -99,17 +98,15 @@ class dns():
             "208.67.222.222", "208.67.220.220"   # OpenDNS
         ]
 
-    def resolve(self, domain):
-        # resolves certificate records with caching
-        if domain in self.cache:
-            return self.cache[domain]
+    def resolve(self, domain: str):
         try:
-            result = self.resolver.resolve(domain, 'CNAME')
-            self.cache[domain] = result
-            return result
-        except Exception:
-            self.cache[domain] = None
+            self.resolver.resolve(domain, 'CNAME')
+            return 200
+        except dns.resolver.AccessDenied:
+            return 403
+        except:
             return None
+
 
 class Saifandor():
     def __init__(self):
@@ -140,104 +137,142 @@ class Saifandor():
             exit_program()
 
     def fetch_records(self) -> None:
-        working_indicator = Halo(text="fetching subdomain information", spinner="bar")
+        working_indicator = Halo(text="fetching subdomain information", spinner="bouncingBar")
         target_url = self.clean_url()
+
+        working_indicator.start()
 
         api_url = f"https://crt.sh/?q=%25.{target_url}&output=json"
         headers = {"User-Agent": random.choice(user_agents)}
 
-        working_indicator.start()
         response = requests.get(url=api_url, headers=headers, timeout=30)
-        records = response.json()
-        self.records = records
+
+        if response.status_code == 200:
+            self.records = response.json()
+        else:
+            print(f"{Fore.RED}[!] Error: fetch failed with status code: {Fore.YELLOW}{response.status_code}{Fore.RESET}")
+            exit_program()
+        
+        self.process_records()
+
+        self.check_status_codes()
+
         working_indicator.stop()
 
-        processed_records = self.process_records()
-        print(processed_records)
 
-    def extract_subdomains(self, data: str) -> list:
-        domains = []
 
-        data = data.split()
-        data = data.split("\n")
-        
-        for chunk in data:
-            extracted = tldextract.extract(chunk)
-            if extracted.domain:
-                domains.append(f"{extracted.domain}.{extracted.suffix}")
-        
-        return list(set(domains))
 
-    # Doesn't work. Cause of error
-    def extract_emails(self, data: str) -> list:
-        extractor = Extractor
-        extracted_data = Extractor.extract(data)
-        emails = extracted_data.get('emails', [])
-        return emails
-    
-    def extract_certificate_data(self, record_data: dict) -> dict:
-        cert_info = {}
-
-        # Extract the issuer name and issue (not_before) date directly from the record_data
-        issuer_name = record_data["issuer_name"]
-        not_before = record_data["not_before"]
-
-        # searches for the CA name
-        ca_name_match = re.search(r'CN=([^,]+)', issuer_name)
-        if ca_name_match != None:
-            cert_info['ca_name'] = ca_name_match.group(1).strip()
-        else:
-            cert_info = {"ca_name": None}
-
-        # extracts the date from the not_before date (issue_date)
-        not_before = not_before.strip()
-        issue_date_pattern = r'(\d{4}-\d{2}-\d{2})T\d{2}:\d{2}:\d{2}'
-        issue_date = re.search(issue_date_pattern, not_before)
-        cert_info["issue_date"] = issue_date.group(1).strip()
-
-        return cert_info
+    """
+    {
+        {
+            'issuer_ca_id': 4, 
+            'issuer_name': 'C=US, O=Google Inc, CN=Google Internet Authority',
+            'common_name': 'freezone.google.com', 
+            'name_value': 'accounts.freezone.google.com\nfreezone.google.com\ngaiastaging.freezone.google.com\nmail.freezone.google.com\nnews.freezone.google.com\nplus.freezone.google.com\nsearch.freezone.google.com\nwww.freezone.google.com', 
+            'id': 1708824, 'entry_timestamp': '2013-04-27T12:11:35.274', 
+            'not_before': '2013-04-24T15:07:26', 
+            'not_after': '2013-12-30T15:07:26', 
+            'serial_number': '1403460a000100008520', 
+            'result_count': 9
+        }
+    }
+    """
 
     def process_records(self) -> list:
-        cleaned_records = []
+        record_data = {"subdomains": None, "emails": None, "CA_names": None, "cert_issue_dates": None}
+        email_and_subdomain_data = []
+        CA_name_data = []
+        cert_issue_date_data = []
 
         records = self.records
-        for index, record in enumerate(records):
-            # domain fields
+        for record in records:
             common_name = record["common_name"]
             name_value = record["name_value"]
-            # relevant certificate information fields
-            rel_record_date = {"issuer_name": record["issuer_name"], "not_before": record["not_before"]}
+            issuer_name = record["issuer_name"]
+            cert_issue_date = record["not_before"]
 
-            # extracts the subdomain(s) from the current record
-            subdomains = []
-            common_name_subdomains = [sub for sub in self.extract_subdomains(data=common_name)]
-            for sub in common_name_subdomains:
-                subdomains.append(sub)
+            email_and_subdomain_data.append(common_name)
+            email_and_subdomain_data.append(name_value)
+            CA_name_data.append(issuer_name)
+            cert_issue_date_data.append(cert_issue_date)
+        
+        subdomains = []
+        emails = []
+        for entry in email_and_subdomain_data:
+            data = entry.split("\n")
+            for chunk in data:
+                if "@" in chunk:
+                    emails.append(chunk.strip())
+                else:
+                    subdomains.append(chunk.strip())
+        
+        CA_names = []
+        for entry in CA_name_data:
+            data = entry.split(",")
+            for chunk in data:
+                if chunk.startswith("CN="):
+                    chunk = chunk.strip("CN=")
+                    chunk = chunk.strip()
+                    CA_names.append(chunk)
+                else:
+                    continue
+        
+        cert_issue_dates = []
+        for entry in cert_issue_date_data:
+            cert_issue_dates.append(entry.split("T")[0])
+        
+        record_data["subdomains"] = subdomains
+        record_data["emails"] = emails
+        record_data["CA_names"] = CA_names
+        record_data["cert_issue_dates"] = cert_issue_dates
 
-            name_value_subdomains = [sub for sub in self.extract_subdomains(data=name_value)]
-            for sub in name_value_subdomains:
-                subdomains.append(sub)
+        self.records = record_data
 
-            # extracts the email(s) from the current record
-            emails = []
-            common_name_emails = [email for email in self.extract_emails(data=common_name)]
-            for email in common_name_emails:
-                emails.append(email)
-            
-            name_value_emails = [email for email in self.extract_emails(data=name_value)]
-            for email in name_value_emails:
-                emails.append(email)
-            
-            # NOTE: The CA_name and issue data will be the same for all subdomains/emails found for the current record
-            certificate_info = self.extract_certificate_data(record_data=rel_record_date)
-            try:
-                ca_name = certificate_info["ca_name"]
-            except KeyError:
-                print(certificate_info)
-            issue_date = certificate_info["issue_date"]
+    def check_status_codes(self):
+        dns = DNS()
+        records = self.records
+        status_codes = []
 
-            cleaned_records.append({"discovered_subdomains": subdomains, "CA": ca_name, "issue_date": issue_date})
+        for index, subdomain in enumerate(records["subdomains"], start=0):
+            status_code = dns.resolve(domain=subdomain)
+            if status_code != None:
+                status_codes.append(status_code)
+            else:
+                print(f"\nBye Bye {subdomain}")
+                records["subdomains"].pop(index)
+                records["CA_names"].pop(index)
+                records["cert_issue_dates"].pop(index)
+
+
+
+
+def display_header():
+    ascii_banner = """
+     _____       _  __                _            
+    /  ___|     (_)/ _|              | |           
+    \\ `--.  __ _ _| |_ __ _ _ __   __| | ___  _ __ 
+     `--. \\/ _` | |  _/ _` | '_ \\ / _` |/ _ \\| '__|
+    /\\__/ / (_| | | || (_| | | | | (_| | (_) | |   
+    \\____/ \\__,_|_|_| \\__,_|_| |_|\\__,_|\\___/|_|   """
+    title_colors = [Fore.RED, Fore.YELLOW, Fore.WHITE, Fore.GREEN]
+    colorful_banner = ''.join(title_colors[char % len(title_colors)] + ascii_banner[char] for char in range(len(ascii_banner)))
+    
+    title_bar = f"{Fore.YELLOW}_________________________________________________________________________/{Fore.RESET}"
+
+    header = f"{colorful_banner}\n{title_bar}\n{Fore.BLUE}Saifandor v1.0{Fore.RESET}"
+    print(header)
 
 def test():
+    working_indicator = Halo(text="fetching subdomain information", spinner="bouncingBar")
     saifandor = Saifandor()
+    display_header()
+    working_indicator.start()
     saifandor.fetch_records()
+
+# TODO:
+
+"""
+1) Find the domains, emails, CA Name, and the certificate issue date in data
+2) Check the status code of each domain (200, 403, 401, and 408 are good)
+3) Finish the output display
+"""
