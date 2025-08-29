fa7375371b445130fd2daf42644d43d97f81d855
Madipadige-ManishKumar <oomanish459@gmail.com>
2025-08-29 15:15:21 +0530

    Updated Serikandor.py

--- Interfaces/hsmi.py
+++ Interfaces/hsmi.py
@@ -111,7 +111,7 @@ if __name__ == "__main__":
 
     # checks the tool and determines which one to call
     if settings["tool"] == "serikandor":
-        # tool_output = serikandor.run()
+        tool_output = serikandor.run()
         print(settings["target"])
     elif settings["tool"] == "patchpirate":
         print(
